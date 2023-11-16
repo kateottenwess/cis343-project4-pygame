d303a9fb3cbaf16eadb9090bfb360cc560c88ef6
Kate Ottenwess <90808295+kateottenwess@users.noreply.github.com>
2023-11-16 15:29:21 -0500

    started very basic main file

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame as pg
+
+
+def main():
+    # start pygame
+    pg.init()
+
+    # get screen object
+    screen = pg.display.set_mode((1024, 768))
+
+    running = True
+
+    while running:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+
+        # update things that need to be updated
+
+        # draw
+
+        pg.display.flip()
+
+
+main()
