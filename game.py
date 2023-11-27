8ec6374faf244e509230b6887e1922da72d5be97
Kate Ottenwess <90808295+kateottenwess@users.noreply.github.com>
2023-11-26 20:12:54 -0500

    got background up

--- game.py
+++ game.py
@@ -1,9 +1,14 @@
+import pygame
 import pygame as pg
 from pygame.locals import *
 
-class Game():
-    def __init__(self):
+class Game(pygame.sprite.Sprite):
+    def __init__(self, image_file, location):
         value = 1
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(image_file)
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = location
 
 
 def main():
@@ -11,7 +16,8 @@ def main():
     pg.init()
     
     # get screen object
-    screen = pg.display.set_mode((1024, 768))
+    screen = pg.display.set_mode((960, 720))
+    bg = Game('./assets/frogger-background.png', [0, 0])
     running = True
    # instantiate game object here!
     #clock =
@@ -23,13 +29,15 @@ def main():
         # update things that need to be updated
 
         # draw & re render outputs
-        screen.fill(255, 255, 255)
+        screen.fill([255, 255, 255])
+        screen.blit(bg.image, bg.rect)
         #our_go.draw(screen)
         pg.display.flip()
+        clock = pg.time.Clock()
         delta = clock.tick(60)/1000     # gives us how many ms the frame has taken
        
         pg.display.flip()
 
 
-main()
-
+if __name__ == "__main__":
+    main()
